3d262d965efac8f96ebd69f6e266a840227bd1ee
Will2611 <william.ongjiajiang@gmail.com>
2020-12-18 13:25:33 +0000

    better recurring tTest function, hopefully

--- ttest2_samples.m
+++ ttest2_samples.m
@@ -1,11 +1,17 @@
 function ttest_res=ttest2_samples(classification_accs)
-ttest_res=zeros(size(classification_accs,1),2)
+ttest_res=zeros(10,2)
 res_index=1;
+fprintf("1=ANN\n");
+fprintf("2=DT\n");
+fprintf("3=SVM: Linera\n");
+fprintf("4=SVM: Polynomial\n");
+fprintf("5=SVM: RBF\n");
  for i=1:size(classification_accs,2)
      for j=i+1:size(classification_accs,2)
-         fprintf("i=%d, k = %d\n",i,j);
-         [h p] = ttest2(classification_accs(i,:),classification_accs(j,:));
+         fprintf("Comparing between column %d and %d\n",i,j);
+         [h p] = ttest2(classification_accs(:,i),classification_accs(:,j));
          ttest_res(res_index,:)=[h p];
+         ttest_res(res_index,:)
          res_index=res_index+1;
      end
  end
